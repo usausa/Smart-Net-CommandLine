87105617d9fccedbbf42b778a3c81aad70af0d85
usausa <machi.pon@gmail.com>
2026-09-01 11:13:52 +0900

    Update

--- Smart.CommandLine.Hosting/ICommandFilter.cs
+++ Smart.CommandLine.Hosting/ICommandFilter.cs
@@ -2,7 +2,5 @@ namespace Smart.CommandLine.Hosting;
 
 public interface ICommandFilter
 {
-#pragma warning disable CA1716
     ValueTask ExecuteAsync(CommandContext context, CommandDelegate next);
-#pragma warning restore CA1716
 }
